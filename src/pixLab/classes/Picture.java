0c8aef374957f0070726bf4a49a6f2114f558904
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2018-01-26 09:55:55 -0700

    save

--- src/pixLab/classes/Picture.java
+++ src/pixLab/classes/Picture.java
@@ -173,6 +173,20 @@ public class Picture extends SimplePicture
       }
     }   
   }
+  
+  public void gengarFilter(int startRow, int startCol)
+  {
+	  Pixel fromPixel = null;
+	  Pixel toPixel = null;
+	  Picture gengar = new Picture("gengar.png");
+	  
+  }
+  
+  
+  
+  
+  
+  
 
   /** Method to create a collage of several pictures */
   public void createCollage()
