bf6179b7b3ff3a49af4a937ae7e18df02117b4be
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2018-02-13 09:51:52 -0700

    saving

--- src/pixLab/classes/classFilter.java
+++ src/pixLab/classes/classFilter.java
@@ -1,5 +1,19 @@
 package pixLab.classes;
+import java.awt.*;
+import java.awt.font.*;
+import java.awt.geom.*;
+import java.awt.image.BufferedImage;
+import java.text.*;
+import java.util.*;
+import java.util.List;
 
-public class classFilter {
-
+public class classFilter 
+{
+	for (int row = 0; row < 20; row++)
+	{
+		for (int col = 0; col < 20; col++)
+		{
+			
+		}
+	}
 }
